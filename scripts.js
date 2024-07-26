07922f0dde1a64f094064432886e48917a2652c4
NhatAnh564 <nhatanhngo0506@gmail.com>
2024-07-26 14:31:10 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.style.display = 'block';
+}
+
+// Show the first section by default
+showSection('objective');
